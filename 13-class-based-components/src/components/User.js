d0b4b49261cf06e6099f0438f499123150078e31
Evan He <evanhe27@gmail.com>
2021-11-02 17:03:05 -0700

    finished section 13 of class, begin switch to yarn

--- 13-class-based-components/src/components/User.js
+++ 13-class-based-components/src/components/User.js
@@ -1,7 +1,19 @@
-import classes from './User.module.css';
+import React from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from "./User.module.css";
+
+class User extends React.Component {
+  componentWillUnmount() {
+    console.log("User will unmount");
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+//const User = (props) => {
+//  return <li className={classes.user}>{props.name}</li>;
+//};
 
 export default User;
--- 13-class-based-components/src/components/Users.js
+++ 13-class-based-components/src/components/Users.js
@@ -1,14 +1,46 @@
-import { useState } from 'react';
-import User from './User';
+import React, { Component, useState } from "react";
+import User from "./User";
 
-import classes from './Users.module.css';
+import classes from "./Users.module.css";
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
 
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error("No users provided!");
+    }
+  }
+
+  toggleUsersHandler() {
+    this.setState((curState) => ({ showUsers: !curState.showUsers }));
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+/*
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -27,11 +59,11 @@ const Users = () => {
   return (
     <div className={classes.users}>
       <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
+        {showUsers ? "Hide" : "Show"} Users
       </button>
       {showUsers && usersList}
     </div>
   );
 };
-
+*/
 export default Users;
